a77eda6fbcd2293575f3c33456ccdf25e1087ec4
set404 <89992008819@mail.ru>
2023-03-07 22:18:55 +0300

    Add AuthControllerTest

---
+++ src/test/java/ru/set404/clients/controllers/AuthControllerTest.java
@@ -0,0 +1,90 @@
+package ru.set404.clients.controllers;
+
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import ru.set404.clients.ClientsApplication;
+import ru.set404.clients.dto.TherapistDTO;
+import ru.set404.clients.security.JwtRequest;
+import ru.set404.clients.services.TherapistService;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = {ClientsApplication.class})
+@AutoConfigureMockMvc
+@EnableAutoConfiguration(exclude = SecurityAutoConfiguration.class)
+@TestPropertySource(locations = "classpath:application-test.properties")
+@ActiveProfiles("test")
+@Sql(scripts = {"classpath:delete-data.sql", "classpath:init-data.sql"})
+public class AuthControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+    @Autowired
+    private TherapistService service;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    public void whenLoginValidCredentials() throws Exception {
+        TherapistDTO therapist = createTestTherapist();
+
+        JwtRequest request = new JwtRequest();
+        request.setLogin(therapist.getPhone());
+        request.setPassword(therapist.getPassword());
+
+        mvc.perform(post("/auth/login").contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().is(200)).andExpect(jsonPath("$.accessToken").exists());
+    }
+
+    @Test
+    public void whenLoginInvalidUsername() throws Exception {
+        TherapistDTO therapist = createTestTherapist();
+
+        JwtRequest request = new JwtRequest();
+        request.setLogin("000" + therapist.getPhone());
+        request.setPassword(therapist.getPassword());
+
+        mvc.perform(post("/auth/login").contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().is(403)).andExpect(jsonPath("$.message", is("Authorization error. User with phone - " + request.getLogin() + " not found")));
+    }
+
+    @Test
+    public void whenLoginInvalidPassword() throws Exception {
+        TherapistDTO therapist = createTestTherapist();
+
+        JwtRequest request = new JwtRequest();
+        request.setLogin(therapist.getPhone());
+        request.setPassword("Invalid" + therapist.getPassword());
+
+        mvc.perform(post("/auth/login").contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().is(403)).andExpect(jsonPath("$.message", is("Authorization error. Wrong password")));
+    }
+
+    private TherapistDTO createTestTherapist() {
+        TherapistDTO therapist = new TherapistDTO("Bob", "88005553535", "qwerty");
+        service.saveTherapist(therapist);
+        return therapist;
+    }
+}
---
+++ src/test/resources/delete-data.sql
@@ -0,0 +1,5 @@
+DROP TABLE IF EXISTS appointments;
+DROP TABLE IF EXISTS services;
+DROP TABLE IF EXISTS availability;
+DROP TABLE IF EXISTS therapists;
+DROP TABLE IF EXISTS clients;
