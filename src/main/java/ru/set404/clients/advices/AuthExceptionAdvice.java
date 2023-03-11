6322b995cee65a03092ab0d848922b91e8acb766
set404 <89992008819@mail.ru>
2023-03-11 22:00:45 +0300

    Refactoring exception advices

---
+++ src/main/java/ru/set404/clients/advices/AuthExceptionAdvice.java
@@ -0,0 +1,31 @@
+package ru.set404.clients.advices;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
+import jakarta.security.auth.message.AuthException;
+import org.springdoc.api.ErrorMessage;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class AuthExceptionAdvice {
+
+    @ResponseBody
+    @ExceptionHandler(AuthException.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public ResponseEntity<Object>  authExceptionHandler(AuthException ex) {
+        return new ResponseEntity<>(new ErrorMessage("Authorization error. " + ex.getMessage()), new HttpHeaders(), HttpStatus.FORBIDDEN);
+
+    }
+
+    @ExceptionHandler(value = {JwtException.class})
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public ResponseEntity<Object> handleExpiredJwtException(ExpiredJwtException ex) {
+        return new ResponseEntity<>(new ErrorMessage(ex.getMessage()), new HttpHeaders(), HttpStatus.FORBIDDEN);
+    }
+}
--- src/main/java/ru/set404/clients/advices/RequestExceptionAdvice.java
+++ src/main/java/ru/set404/clients/advices/RequestExceptionAdvice.java
@@ -1,11 +1,8 @@
 package ru.set404.clients.advices;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.JwtException;
 import jakarta.security.auth.message.AuthException;
-import lombok.RequiredArgsConstructor;
 import org.springdoc.api.ErrorMessage;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -17,29 +14,11 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.context.request.WebRequest;
 import ru.set404.clients.exceptions.TimeNotAvailableException;
 import ru.set404.clients.exceptions.UserAlreadyExistException;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @ControllerAdvice
-public class ExceptionAdvice {
-
-    @ResponseBody
-    @ExceptionHandler(AuthException.class)
-    @ResponseStatus(HttpStatus.FORBIDDEN)
-    public ResponseEntity<Object>  authExceptionHandler(AuthException ex) {
-        return new ResponseEntity<>(new ErrorMessage("Authorization error. " + ex.getMessage()), new HttpHeaders(), HttpStatus.FORBIDDEN);
-
-    }
-
-    @ExceptionHandler(value = {JwtException.class})
-    @ResponseStatus(HttpStatus.FORBIDDEN)
-    public ResponseEntity<Object> handleExpiredJwtException(ExpiredJwtException ex, WebRequest request) {
-        return new ResponseEntity<>(new ErrorMessage(ex.getMessage()), new HttpHeaders(), HttpStatus.FORBIDDEN);
-    }
+public class RequestExceptionAdvice {
 
     @ExceptionHandler(value = {HttpRequestMethodNotSupportedException.class})
     @ResponseStatus(HttpStatus.FORBIDDEN)
@@ -47,21 +26,15 @@ public class ExceptionAdvice {
         return new ResponseEntity<>(new ErrorMessage(ex.getMessage()), new HttpHeaders(), HttpStatus.FORBIDDEN);
     }
 
-    @ExceptionHandler(value = {TimeNotAvailableException.class})
-    @ResponseStatus(HttpStatus.NOT_ACCEPTABLE)
-    public ResponseEntity<Object> requestNotSupportedException(TimeNotAvailableException ex) {
-        return new ResponseEntity<>(new ErrorMessage(ex.getMessage()), new HttpHeaders(), HttpStatus.NOT_ACCEPTABLE);
-    }
-
     @ExceptionHandler(value = {UserAlreadyExistException.class})
     @ResponseStatus(HttpStatus.NOT_ACCEPTABLE)
-    public ResponseEntity<Object> requestNotSupportedException(UserAlreadyExistException ex) {
+    public ResponseEntity<Object> userAlreadyExistException(UserAlreadyExistException ex) {
         return new ResponseEntity<>(new ErrorMessage(ex.getMessage()), new HttpHeaders(), HttpStatus.NOT_ACCEPTABLE);
     }
 
     @ExceptionHandler(value = {MethodArgumentNotValidException.class})
     @ResponseStatus(HttpStatus.NOT_ACCEPTABLE)
-    public ResponseEntity<Object> requestNotSupportedException(MethodArgumentNotValidException ex) throws JsonProcessingException {
+    public ResponseEntity<Object> methodArgumentNotValidException(MethodArgumentNotValidException ex) {
 
         StringBuilder builder = new StringBuilder();
         ex.getBindingResult().getAllErrors().forEach((error) -> {
