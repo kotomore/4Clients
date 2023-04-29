b46a5590600715298e795f4847ea2588d250cd52
set404 <89992008819@mail.ru>
2023-04-29 19:57:59 +0300

    Add ManagementService tests

--- management-service/src/test/java/ru/set404/clients/services/ManagementServiceTest.java
+++ management-service/src/test/java/ru/set404/clients/services/ManagementServiceTest.java
@@ -1,113 +1,191 @@
 package ru.set404.clients.services;
 
-import org.junit.Assert;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import ru.set404.clients.dto.AgentDTO;
+import ru.set404.clients.exceptions.AgentNotFoundException;
 import ru.set404.clients.exceptions.AppointmentNotFoundException;
-import ru.set404.clients.models.Appointment;
-import ru.set404.clients.models.Schedule;
-import ru.set404.clients.models.TimeSlot;
+import ru.set404.clients.exceptions.TimeNotAvailableException;
+import ru.set404.clients.models.*;
+import ru.set404.clients.repositories.AgentRepository;
 import ru.set404.clients.repositories.AppointmentRepository;
-import ru.set404.clients.repositories.ScheduleRepository;
+import ru.set404.clients.repositories.AvailabilityRepository;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.*;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.when;
-
-class ManagementServiceTest {
-
+@ExtendWith(MockitoExtension.class)
+public class ManagementServiceTest {
     @InjectMocks
     private ManagementService managementService;
 
     @Mock
-    private ScheduleRepository scheduleRepository;
+    private AgentRepository agentRepository;
 
     @Mock
     private AppointmentRepository appointmentRepository;
 
-    private static final String AGENT_ID = "123";
-    private static final String APPOINTMENT_ID = "456";
+    @Mock
+    private AvailabilityRepository availabilityRepository;
 
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @Test
+    void findAllAppointments() {
+        Mockito.when(appointmentRepository.findByAgentId("agent-123")).thenReturn(List.of(new Appointment()));
+
+        List<Appointment> appointments = managementService.findAllAppointments("agent-123");
+        Assertions.assertNotNull(appointments);
     }
 
     @Test
-    public void testFindAllAppointments() {
-        String agentId = "123";
-        Appointment appointment1 = new Appointment();
-        Appointment appointment2 = new Appointment();
-        List<Appointment> expectedAppointments = new ArrayList<>();
-        expectedAppointments.add(appointment1);
-        expectedAppointments.add(appointment2);
+    void findAllAppointmentsThrowsException() {
+        Mockito.when(appointmentRepository.findByAgentId("agent-123")).thenReturn(List.of());
 
-        when(appointmentRepository.findByAgentId(agentId)).thenReturn(expectedAppointments);
-
-        List<Appointment> actualAppointments = managementService.findAllAppointments(agentId);
-
-        Assert.assertEquals(expectedAppointments, actualAppointments);
+        Assertions.assertThrows(AppointmentNotFoundException.class,
+                () -> managementService.findAllAppointments("agent-123"));
     }
 
     @Test
-    public void testFindAppointmentById() {
-        String agentId = "123";
-        String appointmentId = "456";
-        Appointment expectedAppointment = new Appointment();
+    void findAppointmentById() {
+        Appointment appointment = new Appointment();
+        appointment.setServiceId("appointment-123");
+        appointment.setAgentId("agent-123");
+        appointment.setClient(new Client());
+        appointment.setTimeSlot(new TimeSlot());
 
-        Mockito.when(appointmentRepository.findByIdAndAgentId(appointmentId, agentId)).thenReturn(Optional.of(expectedAppointment));
+        Mockito.when(appointmentRepository.findByIdAndAgentId("appointment-123", "agent-123"))
+                .thenReturn(Optional.of(appointment));
+        Appointment createdAppointment = managementService.findAppointmentById("agent-123", "appointment-123");
 
-        Appointment actualAppointment = managementService.findAppointmentById(agentId, appointmentId);
-
-        Assert.assertEquals(expectedAppointment, actualAppointment);
+        Assertions.assertNotNull(appointment);
+        Assertions.assertEquals(appointment, createdAppointment);
     }
 
     @Test
-    public void testFindAvailableDates() {
-        String agentId = "123";
-        LocalDate date = LocalDate.now();
-        List<LocalDate> expectedDates = new ArrayList<>();
-        expectedDates.add(LocalDate.now());
-        expectedDates.add(LocalDate.now().plusDays(1));
+    void findAppointmentByIdThrowsException() {
+        Mockito.when(appointmentRepository.findByIdAndAgentId("appointment-123", "agent-123"))
+                .thenReturn(Optional.empty());
 
-        List<TimeSlot> timeSlots = new ArrayList<>();
-        timeSlots.add(new TimeSlot(LocalTime.of(9, 0), LocalTime.of(10, 0)));
-        timeSlots.add(new TimeSlot(LocalTime.of(10, 0), LocalTime.of(11, 0)));
-        timeSlots.add(new TimeSlot(LocalTime.of(11, 0), LocalTime.of(12, 0)));
-
-        Mockito.when(scheduleRepository.findByAgentIdAndDateGreaterThanEqual(agentId, date)).thenReturn(
-                Arrays.asList(new Schedule("1", agentId, date, timeSlots), new Schedule("id", agentId, date.plusDays(1), timeSlots)));
-
-        Set<LocalDate> actualDates = managementService.findAvailableDates(agentId, date);
-
-        Assert.assertEquals(expectedDates, actualDates);
+        Assertions.assertThrows(AppointmentNotFoundException.class,
+                () -> managementService.findAppointmentById("agent-123", "appointment-123"));
     }
 
     @Test
-    public void testFindAvailableTimes() {
-        String agentId = "123";
-        LocalDate date = LocalDate.now();
-        List<LocalTime> expectedTimes = Arrays.asList(LocalTime.of(9, 0), LocalTime.of(10, 0), LocalTime.of(11, 0));
+    void deleteAppointment() {
 
-        Schedule schedule = new Schedule();
-        List<TimeSlot> timeSlots = new ArrayList<>();
-        timeSlots.add(new TimeSlot(LocalTime.of(9, 0), LocalTime.of(10, 0)));
-        timeSlots.add(new TimeSlot(LocalTime.of(10, 0), LocalTime.of(11, 0)));
-        timeSlots.add(new TimeSlot(LocalTime.of(11, 0), LocalTime.of(12, 0)));
-        schedule.setAvailableSlots(timeSlots);
+        //Add appointment
+        TimeSlot timeSlot = new TimeSlot();
+        timeSlot.setStartTime(LocalTime.of(22, 0));
+        timeSlot.setEndTime(LocalTime.of(23, 0));
 
-        Mockito.when(scheduleRepository.findByAgentIdAndDate(agentId, date)).thenReturn(Optional.of(schedule));
+        Appointment appointment = new Appointment();
+        appointment.setServiceId("appointment-123");
+        appointment.setAgentId("agent-123");
+        appointment.setClient(new Client());
+        appointment.setTimeSlot(timeSlot);
 
-        Set<LocalTime> actualTimes = managementService.findAvailableTimes(agentId, date);
+        //Add availability
+        Availability availability = new Availability();
+        availability.setDate(LocalDate.now());
+        availability.setStartTime(LocalTime.of(22, 0));
+        availability.setEndTime(LocalTime.of(23, 0));
+        availability.setAgentId("agent-123");
 
-        Assert.assertEquals(expectedTimes, actualTimes);
+
+
+        Mockito.when(appointmentRepository.findByIdAndAgentId("appointment-123", "agent-123"))
+                .thenReturn(Optional.of(appointment));
+        Mockito.when(availabilityRepository.findByAgentIdAndDateAndStartTime(
+                appointment.getAgentId(),
+                appointment.getDate(),
+                appointment.getTimeSlot().getStartTime())).thenReturn(Optional.of(availability));
+
+
+        managementService.deleteAppointment("agent-123", "appointment-123");
+
+        Mockito.verify(appointmentRepository).deleteByIdAndAgentId("appointment-123", "agent-123");
+    }
+
+    @Test
+    void deleteAppointmentThrowsException() {
+        Mockito.when(appointmentRepository.findByIdAndAgentId("appointment-123", "agent-123"))
+                .thenReturn(Optional.empty());
+        Assertions.assertThrows(AppointmentNotFoundException.class,
+                () -> managementService.deleteAppointment("agent-123", "appointment-123"));
+    }
+
+    @Test
+    void findAgentById() {
+        Agent agent = new Agent();
+        agent.setPhone("123-456-7890");
+        agent.setName("John");
+        agent.setPassword("password");
+        Mockito.when(agentRepository.findById("agent-123")).thenReturn(Optional.of(agent));
+        Agent updatedAgent = managementService.findAgentById("agent-123");
+        Assertions.assertNotNull(agent);
+        Assertions.assertEquals(agent, updatedAgent);
+    }
+
+    @Test
+    void findAgentByIdThrowsException() {
+        Mockito.when(agentRepository.findById("agent-123")).thenReturn(Optional.empty());
+
+        Assertions.assertThrows(AgentNotFoundException.class,
+                () -> managementService.findAgentById("agent-123"));
+    }
+
+    @Test
+    void updateAgent() {
+        Agent agent = new Agent();
+        agent.setPhone("123-456-7890");
+        agent.setName("John");
+        agent.setPassword("password");
+
+
+        Mockito.when(agentRepository.findById("agent-123")).thenReturn(Optional.of(agent));
+        Mockito.when(passwordEncoder.encode("password")).thenReturn("encoded password");
+
+        managementService.updateAgent("agent-123", new AgentDTO("Agent updated name", "123", "password"));
+        Mockito.verify(agentRepository).save(agent);
+    }
+
+    @Test
+    void updateAgentThrowsException() {
+        Mockito.when(agentRepository.findById("agent-123")).thenReturn(Optional.empty());
+
+        Assertions.assertThrows(AgentNotFoundException.class,
+                () -> managementService.updateAgent("agent-123", new AgentDTO()));
+    }
+
+    @Test
+    void findAvailableTimes() {
+        Availability availability = new Availability();
+        availability.setDate(LocalDate.now());
+        availability.setStartTime(LocalTime.of(22, 0));
+        availability.setEndTime(LocalTime.of(23, 0));
+        availability.setAgentId("agent-123");
+
+        Mockito.when(availabilityRepository.findByAgentIdAndDate("agent-123", LocalDate.now())).thenReturn(List.of(availability));
+        Set<LocalTime> times = managementService.findAvailableTimes("agent-123", LocalDate.now());
+        Assertions.assertEquals(1, times.size());
+    }
+
+    @Test
+    void findAvailableTimesThrowsException() {
+        Mockito.when(availabilityRepository.findByAgentIdAndDate("agent-123", LocalDate.now())).thenReturn(List.of());
+
+        Assertions.assertThrows(TimeNotAvailableException.class,
+                () -> managementService.findAvailableTimes("agent-123", LocalDate.now()));
     }
 }
